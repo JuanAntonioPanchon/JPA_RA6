e3694a09c009f0d930a58a6689da166d4b84f514
paulaminguez14 <dominguez.bernabe.paula@iescamas.es>
2025-02-07 10:05:27 +0100

    creada clase Producto, Servicio, repositorio y inicializacion de datos

--- cliente_uno_a_muchos_pedido/src/main/java/com/hlc/cliente_uno_a_muchos_pedido/config/InicializadorDatos.java
+++ cliente_uno_a_muchos_pedido/src/main/java/com/hlc/cliente_uno_a_muchos_pedido/config/InicializadorDatos.java
@@ -1,6 +1,8 @@
 package com.hlc.cliente_uno_a_muchos_pedido.config;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Component;
 import com.github.javafaker.Faker;
 import com.hlc.cliente_uno_a_muchos_pedido.entidad.Cliente;
 import com.hlc.cliente_uno_a_muchos_pedido.entidad.Pedido;
+import com.hlc.cliente_uno_a_muchos_pedido.entidad.Producto;
 import com.hlc.cliente_uno_a_muchos_pedido.repositorio.*;
 
 @Component
@@ -19,6 +22,9 @@ public class InicializadorDatos implements CommandLineRunner  {
 	
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private ProductoRepository productoRepository;
 	
 	private Faker faker = new Faker();
 
@@ -36,9 +42,31 @@ public class InicializadorDatos implements CommandLineRunner  {
                 pedido.setCantidad(faker.number().numberBetween(1, 10));
                 pedido.setCliente(cliente);
                 pedidoRepository.save(pedido);
+                
+                List<Pedido> pedidos = new ArrayList<Pedido>();
+                pedidos.add(pedido);
+                
+                Producto producto1 = new Producto();
+        		producto1.setNombre(faker.commerce().productName());
+        		producto1.setDescripcion(faker.commerce().productName());
+        		producto1.setPeso(faker.number().numberBetween(1, 20));
+        		producto1.setStock(faker.number().numberBetween(1, 100));
+        		producto1.setPedidos(pedidos);
+        		productoRepository.save(producto1);
+        		
+        		Producto producto2 = new Producto();
+        		producto2.setNombre(faker.commerce().productName());
+        		producto2.setDescripcion(faker.commerce().productName());
+        		producto2.setPeso(faker.number().numberBetween(1, 20));
+        		producto2.setStock(faker.number().numberBetween(1, 100));
+        		producto2.setPedidos(pedidos);
+        		productoRepository.save(producto2);
+        		
+                List<Producto> productos = new ArrayList<Producto>();
+                productos.add(producto1);
+                productos.add(producto2);
             }
         }
-		
 	}
 
 }
--- cliente_uno_a_muchos_pedido/src/main/java/com/hlc/cliente_uno_a_muchos_pedido/servicio/PedidoServicio.java
+++ cliente_uno_a_muchos_pedido/src/main/java/com/hlc/cliente_uno_a_muchos_pedido/servicio/PedidoServicio.java
@@ -6,10 +6,10 @@ import com.hlc.cliente_uno_a_muchos_pedido.entidad.Cliente;
 import com.hlc.cliente_uno_a_muchos_pedido.entidad.Pedido;
 
 public interface PedidoServicio {
-	  Pedido guardarPedido(Pedido pedido);
-	    Pedido obtenerPedidoPorId(Long id);
-	    List<Pedido> obtenerTodosLosPedidos();
-	    Pedido actualizarPedido(Long id, Pedido pedido);
-	    void eliminarPedido(Long id);
-		List<Pedido> obtenerPedidosPorCliente(Cliente cliente);
+	Pedido guardarPedido(Pedido pedido);
+    Pedido obtenerPedidoPorId(Long id);
+    List<Pedido> obtenerTodosLosPedidos();
+    Pedido actualizarPedido(Long id, Pedido pedido);
+    void eliminarPedido(Long id);
+	List<Pedido> obtenerPedidosPorCliente(Cliente cliente);
 }
